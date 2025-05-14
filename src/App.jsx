b57cc38b06ea951909c5e25072daef65700e0ca1
Musadi Baloch <musadiqbalouchi321@gmail.com>
2025-05-14 21:16:02 +0500

    we are here section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 
 import './App.css'
-import Headerr from './Components/Section1/Headerr'
-    import {  Routes, Route } from 'react-router-dom';
-import Page1 from './Components/Section1/Page1'
+import Headerr from './Components/Header/Headerr'
+import Page1 from './Components/PageOne/Page1'
+import Page2 from './Components/Pagetwo/Page2';
 
 
 function App() {
@@ -10,10 +10,9 @@ function App() {
   return (  
     <div className='setion1'>
       <Headerr />
-        <Routes>
-       <Route exact path='/'element={<Page1 />} />
+      <Page1 />
+      <Page2 />
 
-        </Routes>
     </div>
   )
 }
---
+++ src/Components/Header/Headerr.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Headerr.css"
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+</style>
+
+function Headerr (){
+    return(
+    <div className="header">
+    <div>
+    <img  className="img1"src="./src/assets/headerimg.png"/>
+    </div>
+    <div className="info">
+    <h2 className="home">Home</h2>
+    <h2 className="about">About Us</h2>
+    <h2 className="product">Our Product</h2>
+    <h2 className="order">Pre-Order</h2>
+    </div>
+    <div className="contact" >
+    <img className="header2img" src="./src/assets/user.png" />
+      <h2 className="cont">Contact Us</h2>
+    </div>
+   
+    </div>
+    )
+}
+export default Headerr
+
