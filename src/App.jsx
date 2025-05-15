ca7790824cd0d6a5b027b774420a03229a8562ef
Musadi Baloch <musadiqbalouchi321@gmail.com>
2025-05-15 22:34:21 +0500

    page3

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Headerr from './Components/Header/Headerr'
 import Page1 from './Components/PageOne/Page1'
 import Page2 from './Components/Pagetwo/Page2';
+import Page3 from './Components/Pagethree/Page3';
 
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
       <Headerr />
       <Page1 />
       <Page2 />
-
+      <Page3 />
     </div>
   )
 }
---
+++ src/Components/Pagethree/Page3.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import "./Page3.css"
+import Card from "./Card";
+import imag1 from "../../assets/Card1img.png"
+import imag2 from "../../assets/Card2.png"
+import imag3 from "../../assets/Card3.png"
+import imag4 from "../../assets/Card4.png"
+import imag5 from "../../assets/Card5.png"
+
+
+const data = [
+    {
+        image:imag1,
+        title:"BAG-IT",
+        detail:"Insulated and re-usable environment friendly grocery shopping bags.",
+        button:"View Button"
+    },{
+        image: imag2,
+        title:"CAN-IT",
+        detail:"Insulated and re-usable environment friendly grocery shopping bags.",
+        button:"View button"
+    },{
+        image: imag3,
+        title:"BOTTLE-IT",
+        detail:"Insulated and re-usable environment friendly grocery shopping bags.",
+        button:"button"
+    },{
+        image:imag4,
+        title:"TRASH-IT",
+        detail:"Insulated and re-usable environment friendly grocery shopping bags.",
+        button:"button"
+    },{
+        image:imag5,
+        title:"TAKE-IT",
+        detail:"Insulated and re-usable environment friendly grocery shopping bags.",
+        button:"button"
+    }
+]
+const Page3 =() =>{
+    return(
+<div className="page3">
+
+    <div className="page3heading">
+        <h1>Presenting Our <span className="h1color">Special Product</span> </h1>
+    </div>
+<div className="fivecards">
+    {/* {data?.length > 0 ? 
+    data?.map((d, index) => {
+        <Card key={index} image={d.image}
+title={d.title}
+detail={d.detail}
+button={d.button} />
+    }): (
+        <p> No data fund</p>
+    )} */}
+    <Card image={imag1} title={"BAG-IT"} detail={"Insulated and re-usable environment friendly grocery."} button={"View button"}/>
+     <Card image={imag2} title={"CAN-IT"} detail={"Insulated and re-usable environment friendly grocery."} button={"View button"}/>
+      <Card image={imag3} title={"BOTTLE-IT"} detail={"Insulated and re-usable environment friendly grocery."} button={"View button"}/>
+       <Card image={imag4} title={"TRASH-IT"} detail={"Insulated and re-usable environment friendly grocery."} button={"View button"}/>
+        <Card image={imag5} title={"TAKE-IT"} detail={"Insulated and re-usable environment friendly grocery ."} button={"View button"}/>
+</div>
+</div>
+    )
+}
+
+export default Page3
