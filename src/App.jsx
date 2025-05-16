90c37736aff2a70ee2d16dec8358cf75c78b4d9f
Musadi Baloch <musadiqbalouchi321@gmail.com>
2025-05-16 22:02:31 +0500

    page4and5

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import Headerr from './Components/Header/Headerr'
 import Page1 from './Components/PageOne/Page1'
 import Page2 from './Components/Pagetwo/Page2';
 import Page3 from './Components/Pagethree/Page3';
-
+import Page4 from './Components/Pagefour/Page4';
+import Page5 from './Components/Pagefive/Page5';
 
 function App() {
 
@@ -14,6 +15,8 @@ function App() {
       <Page1 />
       <Page2 />
       <Page3 />
+      <Page4 />
+      <Page5 />
     </div>
   )
 }
