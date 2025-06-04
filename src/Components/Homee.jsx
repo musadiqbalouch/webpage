9f04a44842a5d0dbc1d414d1f306b0d6fd2807ff
Musadi Baloch <musadiqbalouchi321@gmail.com>
2025-06-04 21:53:51 +0500

    changes for responsive

--- src/Components/Homee.jsx
+++ src/Components/Homee.jsx
@@ -12,13 +12,13 @@ function Homee() {
 
     return (
         <div className='setion1'>
-            <Page1 />
+            {/* <Page1 />
             <Page2 />
-            {/* <Page3 />
+            <Page3 />
             <Page4 />
-            <Page5 />
+            <Page5 /> */}
             <Page6 />
-            <Page7 />
+            {/* <Page7 />
             <Page8 /> */}
         </div>
     )
