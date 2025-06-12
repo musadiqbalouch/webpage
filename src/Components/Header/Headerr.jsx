21f5adddb47bb9baab7273a5c12f0b26843d6cc2
Musadi Baloch <musadiqbalouchi321@gmail.com>
2025-06-12 20:48:38 +0500

    responsive changes

--- src/Components/Header/Headerr.jsx
+++ src/Components/Header/Headerr.jsx
@@ -8,17 +8,17 @@ import { RxHamburgerMenu } from "react-icons/rx";
 
 function Headerr() {
 
-  // const [isOpen, setIsOpen] = useState(false)
-  // function demo() {
-  //   setIsOpen(true)
-  // }
-  // console.log(isOpen)
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+  console.log(isOpen)
   return (
     <div className="header">
-      {/* <div onClick={demo} className="ham">
-        <RxHamburgerMenu />
-      </div > */}
-
+      <div onClick={toggleMenu} className="ham">
+        <RxHamburgerMenu className="hamm" />
+      </div >
       <div>
         <img className="img1" src={img} />
       </div>
@@ -28,15 +28,19 @@ function Headerr() {
         <NavLink to={"/ourproduct"} className="headerlink" > <h2 className="product">Our Product</h2> </NavLink>
         <NavLink to={"/preorders"} className="headerlink" >  <h2 className="order">Pre-Order</h2> </NavLink>
       </div>
-      {/* <div className="info"></div>
-      {isOpen === "info" ? (
-        <>
-          <NavLink to="/" className="headerlink" ><h2 className="home"> Home</h2> </NavLink>
-          <NavLink to={"/about"} className="headerlink" > <h2 className="about">About Us</h2> </NavLink>
-          <NavLink to={"/ourproduct"} className="headerlink" > <h2 className="product">Our Product</h2> </NavLink>
-          <NavLink to={"/preorders"} className="headerlink" >  <h2 className="order">Pre-Order</h2> </NavLink>
-        </>
-      ) : null} */}
+      <div className="inf">
+        {isOpen ? (
+          <>
+            <div className="hamburger">
+              <NavLink to="/" className="headerlink" ><h2 className="home"> Home</h2> </NavLink>
+              <NavLink to={"/about"} className="headerlink" > <h2 className="about">About Us</h2> </NavLink>
+              <NavLink to={"/ourproduct"} className="headerlink" > <h2 className="product">Our Product</h2> </NavLink>
+              <NavLink to={"/preorders"} className="headerlink" >  <h2 className="order">Pre-Order</h2> </NavLink></div>
+          </>
+        ) :
+          (<div className="">
+
+          </div>)}</div>
       <div className="contact" >
         < IoIosContact className="icon" />
         <h2 className="cont">Contact Us</h2>
