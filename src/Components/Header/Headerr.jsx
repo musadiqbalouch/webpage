fc1735f46f63a50a1f8a013c9e5593abffe16d19
musadiqbalouch <musadiqbalouchi321@gmail.com>
2025-06-15 13:14:04 -0700

    changes for responsive

--- src/Components/Header/Headerr.jsx
+++ src/Components/Header/Headerr.jsx
@@ -1,53 +1,72 @@
 import React, { useState } from "react";
-import { NavLink } from 'react-router-dom';
-import "./Headerr.css"
+import { NavLink } from "react-router-dom";
+import "./Headerr.css";
 import { IoIosContact } from "react-icons/io";
-import img from "../.././assets/headerimg.png"
+import img from "../.././assets/headerimg.png";
 import { RxHamburgerMenu } from "react-icons/rx";
 
-
 function Headerr() {
-
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
-  console.log(isOpen)
+  console.log(isOpen);
   return (
     <div className="header">
       <div onClick={toggleMenu} className="ham">
         <RxHamburgerMenu className="hamm" />
-      </div >
+      </div>
       <div>
         <img className="img1" src={img} />
       </div>
       <div className="info">
-        <NavLink to="/" className="headerlink" ><h2 className="home"> Home</h2> </NavLink>
-        <NavLink to={"/about"} className="headerlink" > <h2 className="about">About Us</h2> </NavLink>
-        <NavLink to={"/ourproduct"} className="headerlink" > <h2 className="product">Our Product</h2> </NavLink>
-        <NavLink to={"/preorders"} className="headerlink" >  <h2 className="order">Pre-Order</h2> </NavLink>
+        <NavLink to="/" className="headerlink">
+          <h2 className="home"> Home</h2>{" "}
+        </NavLink>
+        <NavLink to={"/about"} className="headerlink">
+          {" "}
+          <h2 className="about">About Us</h2>{" "}
+        </NavLink>
+        <NavLink to={"/ourproduct"} className="headerlink">
+          {" "}
+          <h2 className="product">Our Product</h2>{" "}
+        </NavLink>
+        <NavLink to={"/preorders"} className="headerlink">
+          {" "}
+          <h2 className="order">Pre-Order</h2>{" "}
+        </NavLink>
       </div>
       <div className="inf">
         {isOpen ? (
           <>
             <div className="hamburger">
-              <NavLink to="/" className="headerlink" ><h2 className="home"> Home</h2> </NavLink>
-              <NavLink to={"/about"} className="headerlink" > <h2 className="about">About Us</h2> </NavLink>
-              <NavLink to={"/ourproduct"} className="headerlink" > <h2 className="product">Our Product</h2> </NavLink>
-              <NavLink to={"/preorders"} className="headerlink" >  <h2 className="order">Pre-Order</h2> </NavLink></div>
+              <NavLink to="/" className="headerlink">
+                <h2 className="home"> Home</h2>{" "}
+              </NavLink>
+              <NavLink to={"/about"} className="headerlink">
+                {" "}
+                <h2 className="about">About Us</h2>{" "}
+              </NavLink>
+              <NavLink to={"/ourproduct"} className="headerlink">
+                {" "}
+                <h2 className="product">Our Product</h2>{" "}
+              </NavLink>
+              <NavLink to={"/preorders"} className="headerlink">
+                {" "}
+                <h2 className="order">Pre-Order</h2>{" "}
+              </NavLink>
+            </div>
           </>
-        ) :
-          (<div className="">
-
-          </div>)}</div>
-      <div className="contact" >
-        < IoIosContact className="icon" />
+        ) : (
+          <div className=""></div>
+        )}
+      </div>
+      <div className="contact">
+        <IoIosContact className="icon" />
         <h2 className="cont">Contact Us</h2>
       </div>
-
-    </div >
-  )
+    </div>
+  );
 }
-export default Headerr
-
+export default Headerr;
--- src/Components/Homee.jsx
+++ src/Components/Homee.jsx
@@ -1,27 +1,25 @@
-
-import Page1 from './PageOne/Page1'
-import Page2 from './Pagetwo/Page2';
-import Page3 from './Pagethree/Page3';
-import Page4 from './Pagefour/Page4';
-import Page5 from './Pagefive/Page5';
-import Page6 from './Pagesix/Page6';
-import Page7 from './Pageseven/Page7';
-import Page8 from './PageEight/Page8';
+import Page1 from "./PageOne/Page1";
+import Page2 from "./Pagetwo/Page2";
+import Page3 from "./Pagethree/Page3";
+import Page4 from "./Pagefour/Page4";
+import Page5 from "./Pagefive/Page5";
+import Page6 from "./Pagesix/Page6";
+import Page7 from "./Pageseven/Page7";
+import Page8 from "./PageEight/Page8";
 
 function Homee() {
-
-    return (
-        <div className='setion1'>
-            <Page1 />
-            <Page2 />
-            <Page3 />
-            <Page4 />
-            <Page5 />
-            <Page6 />
-            <Page7 />
-            <Page8 />
-        </div>
-    )
+  return (
+    <div className="setion1">
+      <Page1 />
+      <Page2 />
+      <Page3 />
+      <Page4 />
+      <Page5 />
+      <Page6 />
+      <Page7 />
+      <Page8 />
+    </div>
+  );
 }
 
-export default Homee
+export default Homee;
